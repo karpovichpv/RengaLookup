0022bf238f5200a8bc9ec36493c94d4ec107cbfb
karpovichpv <karpovich.pavel@gmail.com>
2025-05-08 21:58:03 +0300

    Add the relay command class

---
+++ RengaLookup.UI/Common/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace RengaLookup.UI.Common
+{
+	public class RelayCommand : ICommand
+	{
+		private Action<object> _execute;
+		private Predicate<object> _canExecute;
+
+		public RelayCommand(Action<object> execute, Predicate<object> canExectute)
+		{
+			_execute = execute;
+			_canExecute = canExectute;
+		}
+
+		public event EventHandler CanExecuteChanged
+		{
+			add
+			{
+				CommandManager.RequerySuggested += value;
+			}
+			remove
+			{
+				CommandManager.RequerySuggested -= value;
+			}
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			bool b = _canExecute == null ? true : _canExecute(parameter);
+
+			return b;
+		}
+
+		public void Execute(object parameter)
+		{
+			_execute(parameter);
+		}
+	}
+}
