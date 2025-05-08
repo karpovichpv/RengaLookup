a7159055ab3a6f085ab02d9af575cf294f2753bd
karpovichpv <karpovich.pavel@gmail.com>
2025-05-08 22:08:13 +0300

    Bind the viewModel to the view

--- RengaLookup.UI/ViewModel/ViewModel.cs
+++ RengaLookup.UI/ViewModel/ViewModel.cs
@@ -2,5 +2,16 @@
 {
 	public class ViewModel : ViewModelBase
 	{
+
+		private string _someText = "Some Text";
+		public string SomeText
+		{
+			get => _someText;
+			set
+			{
+				_someText = value;
+				RaisePropertyChange(nameof(SomeText));
+			}
+		}
 	}
 }
