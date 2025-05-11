6d764e24ece312cfca2f61950f09befd93b4cf26
karpovichpv <karpovich.pavel@gmail.com>
2025-05-11 19:50:35 +0300

    Add simple convert for the syntax type

---
+++ RengaLookup.UI/Converters/SyntaxTypeToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using RengaLookup.Model;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RengaLookup.UI.Converters
+{
+	internal class SyntaxTypeToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is SyntaxType syntaxType)
+			{
+				return syntaxType.ToString();
+			}
+
+			return "Undefined type";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return SyntaxType.NotSet;
+		}
+	}
+}
