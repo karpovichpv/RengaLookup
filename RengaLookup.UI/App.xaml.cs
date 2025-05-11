9474d02c6e8f91f849a3cd2276e92bd4a28a9cf4
karpovichpv <karpovich.pavel@gmail.com>
2025-05-11 19:17:56 +0300

    Add di to the UI project

--- RengaLookup.UI/App.xaml.cs
+++ RengaLookup.UI/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Text;
+using System.Windows;
 
 namespace RengaLookup.UI
 {
@@ -7,6 +12,29 @@ namespace RengaLookup.UI
 	/// </summary>
 	public partial class App : Application
 	{
+		public IServiceProvider ServiceProvider { get; private set; }
+		public IConfiguration Configuration { get; private set; }
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", false, true);
+
+			Configuration = builder.Build();
+
+			var serviceCollection = new ServiceCollection();
+			ConfigureServices(serviceCollection);
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+			ServiceProvider = serviceCollection.BuildServiceProvider();
+			MainWindow mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+			mainWindow.Show();
+		}
+
+		private void ConfigureServices(ServiceCollection serviceCollection)
+		{
+			serviceCollection.AddTransient(typeof(MainWindow));
+		}
 
 	}
 }
