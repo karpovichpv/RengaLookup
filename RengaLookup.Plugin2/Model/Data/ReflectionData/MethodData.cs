1f428d01824da8cc28a9fce81be125d6115bac58
karpovichpv <karpovich.pavel@gmail.com>
2026-01-27 21:31:07 +0300

    fix: getting method data

--- RengaLookup.Plugin2/Model/Data/ReflectionData/MethodData.cs
+++ RengaLookup.Plugin2/Model/Data/ReflectionData/MethodData.cs
@@ -13,8 +13,21 @@ namespace RengaLookup.Plugin2.Model.Data.ReflectionData
             _returnType = _info.ReturnType;
             bool isParameterless = _info.GetParameters().Length == 0;
             bool isVoidReturn = _info.ReturnType.Name.Equals("Void", StringComparison.InvariantCulture);
-            if (isParameterless && !isVoidReturn)
+            bool hasForbidenName = CheckIfHasForbidenName(info);
+            if (isParameterless && !isVoidReturn && !hasForbidenName)
                 _returnObject = _info.Invoke(_fatherObject, []);
+        }
+
+        private static bool CheckIfHasForbidenName(MethodInfo info)
+        {
+            return info.Name switch
+            {
+                var s when s.Contains("GetType") => true,
+                var s when s.Contains("MemberwiseClone") => true,
+                var s when s.Contains("GetCopy") => true,
+                var s when s.Contains("GetTypeCode") => true,
+                _ => false
+            };
         }
     }
 }
