b36e2b4ec2200debf3e79e2474edba7763316533
karpovichpv <karpovich.pavel@gmail.com>
2026-02-13 13:01:35 +0300

    feat: exclude xaml code to a separate control

---
+++ Inspector/View/UserControls/DataListBoxControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Inspector.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SelectedObjectsControl.xaml
+    /// </summary>
+    public partial class DataListBoxControl : UserControl
+    {
+        public DataListBoxControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
